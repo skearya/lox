c7b05bc9b32646050e1ca14f78d6318fcea7ee6b
skearya <77034153+skearya@users.noreply.github.com>
2025-04-25 19:18:22 -0400

    parser skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,31 @@
+use std::{collections::VecDeque, iter::Peekable, mem, slice::Iter};
+
+#[derive(Clone, PartialEq, Eq)]
+enum TokenKind {
+    Var,
+    String(String),
+}
+
+struct Parser<I: Iterator<Item = TokenKind>> {
+    tokens: Peekable<I>,
+}
+
+impl<I: Iterator<Item = TokenKind>> Parser<I> {
+    pub fn new(tokens: I) -> Self {
+        Self {
+            tokens: tokens.peekable(),
+        }
+    }
+
+    fn take(&mut self, tokens: &[TokenKind]) -> Option<TokenKind> {
+        let current = self.tokens.peek()?;
+
+        if tokens.iter().any(|token| token == current) {
+            self.tokens.next()
+        } else {
+            None
+        }
+    }
+}
+
 fn main() {}
